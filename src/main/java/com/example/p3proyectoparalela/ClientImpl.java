4c74a0fa5a4b7bae4f089ae6f8a43bfe1d033153
Luis Modesto <a20310068@ceti.mx>
2023-06-29 02:19:25 -0600

    UI cliente funcionando

--- src/main/java/com/example/p3proyectoparalela/ClientImpl.java
+++ src/main/java/com/example/p3proyectoparalela/ClientImpl.java
@@ -2,12 +2,15 @@ package com.example.p3proyectoparalela;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -19,13 +22,13 @@ public class ClientImpl extends Application implements ClientInterface {
     private Label lblClientNumber;
     private Label lblClientStatus;
 
-    public ClientImpl(int clientNumber) {
+    public ClientImpl() {
         this.clientNumber = clientNumber;
         this.clientStatus = "Esperando";
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage)  throws IOException {
         lblClientNumber = new Label("Número de Cliente: " + clientNumber);
         lblClientStatus = new Label("Estado: " + clientStatus);
 
@@ -34,11 +37,12 @@ public class ClientImpl extends Application implements ClientInterface {
         vbox.setPadding(new Insets(20));
         vbox.getChildren().addAll(lblClientNumber, lblClientStatus);
 
+
         Scene scene = new Scene(vbox);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Cliente RMI");
-        primaryStage.show();
-        connectToServer();
+        stage.setScene(scene);
+        stage.setTitle("P3 Proyecto");
+        stage.show();
+        new Thread(this::connectToServer).start();
     }
 
     private void connectToServer() {
@@ -47,6 +51,7 @@ public class ClientImpl extends Application implements ClientInterface {
             server.registerClient(this); // Registrar el cliente en el servidor
             updateClientStatus("Conectado al servidor");
         } catch (Exception e) {
+            updateClientStatus("Error en la conexión del servidor");
             System.out.println("Error al conectarse al servidor: " + e.getMessage());
         }
     }
@@ -69,13 +74,6 @@ public class ClientImpl extends Application implements ClientInterface {
     }
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Se debe proporcionar el número de cliente como argumento.");
-            return;
-        }
-
-        int clientNumber = Integer.parseInt(args[0]);
-        ClientImpl client = new ClientImpl(clientNumber);
-        client.launch(args);
+        launch();
     }
 }
